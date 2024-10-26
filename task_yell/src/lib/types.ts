7bc0c7f975f163cbbf38a996319420728352f7bc
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-10-26 16:51:04 +0900

    add: Task型を追加

--- task_yell/src/lib/types.ts
+++ task_yell/src/lib/types.ts
@@ -9,6 +9,14 @@ export interface Event {
   reccurence: string[];
 }
 
+export interface Task {
+  title: string;
+  description: string;
+  due: Date;
+  importance: "high" | "medium" | "low";
+  reccurence: string[];
+}
+
 export interface Notification {
   datetime: Date;
   type: "call" | "push";
