40db17122406b2d8bf06f9b42ad616e841b2a33f
Ura <mikeneko41@icloudd.com>
2024-10-26 15:34:47 +0900

    イベントのCRUD

---
+++ task_yell/src/lib/events.ts
@@ -0,0 +1,32 @@
+import { Event } from "./types";
+import {
+  createData,
+  readData,
+  readSingleData,
+  updateData,
+  deleteData,
+} from "../firebase/firestore";
+const COLLECTION_NAME = "events";
+
+export async function createEvent(event: Event): Promise<string> {
+  return createData(COLLECTION_NAME, event);
+}
+
+export async function readEvents(): Promise<Event[]> {
+  return readData<Event>(COLLECTION_NAME);
+}
+
+export async function readSingleEvent(id: string): Promise<Event | null> {
+  return readSingleData<Event>(COLLECTION_NAME, id);
+}
+
+export async function updateEvent(
+  id: string,
+  eventData: Partial<Event>
+): Promise<void> {
+  return updateData<Event>(COLLECTION_NAME, id, eventData);
+}
+
+export async function deleteEvent(id: string): Promise<void> {
+  return deleteData(COLLECTION_NAME, id);
+}
