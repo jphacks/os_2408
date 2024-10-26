32b8d5887a20524ad96d768638efdd7f3af54e2c
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-10-26 17:19:49 +0900

    change: createEventで予定のコンフリクトを検知するように変更

--- task_yell/src/lib/events.ts
+++ task_yell/src/lib/events.ts
@@ -1,15 +1,25 @@
-import { Event } from "./types";
 import {
-  createData,
+  deleteData,
   readData,
   readSingleData,
   updateData,
-  deleteData,
 } from "../firebase/firestore";
+import { Event } from "./types";
 const COLLECTION_NAME = "events";
 
-export async function createEvent(event: Event): Promise<string> {
-  return createData(COLLECTION_NAME, event);
+/**
+ * 既存のイベントと衝突しなければ、新しいイベントを作成する。
+ *
+ * @param event 作成されるイベント
+ * @returns 競合がない場合はnull, そうでない場合はマージエディターで必要なので該当日時のイベントのリスト
+ */
+export async function createEvent(event: Event): Promise<Event[] | null> {
+  // event.start ~ event.end とコンフリクトする予定があるかどうかを確認する
+  const events = await readEvents();
+  const isConflict = events.some(
+    (e) => event.start <= e.end && event.end >= e.start
+  );
+  return isConflict ? events : null;
 }
 
 export async function readEvents(): Promise<Event[]> {
@@ -22,7 +32,7 @@ export async function readSingleEvent(id: string): Promise<Event | null> {
 
 export async function updateEvent(
   id: string,
-  eventData: Partial<Event>,
+  eventData: Partial<Event>
 ): Promise<void> {
   return updateData<Event>(COLLECTION_NAME, id, eventData);
 }
