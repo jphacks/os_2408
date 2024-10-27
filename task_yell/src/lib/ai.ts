1eb936e6bd3d315b496b911ec37a64c9e64f6469
Yuto Terada <lugongdao07@gmail.com>
2024-10-27 10:46:52 +0900

    format

--- task_yell/src/lib/ai.ts
+++ task_yell/src/lib/ai.ts
@@ -1,16 +1,18 @@
-import OpenAI from 'openai';
-import { WantTodo } from './types';
+import OpenAI from "openai";
+import { WantTodo } from "./types";
 import { z } from "zod";
 import { zodResponseFormat } from "openai/helpers/zod";
 
 const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const TasksScheme = z.object({
-  tasks: z.array(z.object({
-    content: z.string()
-  }))
+  tasks: z.array(
+    z.object({
+      content: z.string(),
+    }),
+  ),
 });
 
 const prompt = `
@@ -18,27 +20,32 @@ const prompt = `
 出力は元の言語である日本語で出力してください。
 `;
 
-export async function splitWantToDo(wantTodo: WantTodo): Promise<WantTodo[] | null> {
+export async function splitWantToDo(
+  wantTodo: WantTodo,
+): Promise<WantTodo[] | null> {
   try {
     const result = await client.chat.completions.create({
-      model: 'gpt-4o',
+      model: "gpt-4o",
       messages: [
-        { role: 'system', content: prompt },
+        { role: "system", content: prompt },
         {
-          role: 'user', content: `
+          role: "user",
+          content: `
           {
             "content": "${wantTodo.title}"
             }
-            `
-        }
+            `,
+        },
       ],
-      response_format: zodResponseFormat(TasksScheme, 'tasks')
+      response_format: zodResponseFormat(TasksScheme, "tasks"),
     });
 
     console.log(result.choices[0].message.content);
     const content = result.choices[0].message.content;
     if (content) {
-      return JSON.parse(content).tasks.map((item: { content: string }) => ({ title: item.content }));
+      return JSON.parse(content).tasks.map((item: { content: string }) => ({
+        title: item.content,
+      }));
     }
   } catch (e: unknown) {
     console.error(e);
